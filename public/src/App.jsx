a6991e5086eaaffa671e81c05d8e65fc962c474e
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-02-16 18:35:47 -0600

    frontend+backend

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,34 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [users, setUsers] = useState([]);
+  const [id, setId] = useState(0);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000")
+      .then((res) => {
+        setUsers(res.data);
+        setId(0);
+        setName('');
+        setEmail('');
+        setPassword('');
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const submit = (event, id) => {
+    event.preventDefault();
+    if (id === 0) {
+      axios.post("http://127.0.0.1:5000", {
+        name,
+        email,
+        password
+      }).then(() => {
+        fetchData();
+      });
+    } else {
+      axios.put(`http://127.0.0.1:5000/${id}`, {
+        name,
+        email,
+        password
+      }).then(() => {
+        fetchData();
+      });
+    }
+  };
+
+  const deleteData = (id) => {
+    axios.delete(`http://127.0.0.1:5000/${id}`)
+      .then(() => {
+        fetchData();
+      });
+  };
+
+  const edit = (id) => {
+    axios.get(`http://127.0.0.1:5000/edit/${id}`)
+      .then((res) => {
+        setName(res.data.name),
+        setEmail(res.data.email),
+        setPassword(res.data.password),
+        setId(res.data._ID)
+      });
+  };
+
+
+
+  const fetchData = () => {
+    axios.get("http://127.0.0.1:5000")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className='container mt-5'>
+    <div className='row mt-5'>
+      <div className='col lg-6 mt-5'>
+        <form onSubmit={(e) => submit(e, id)}>
+            <div className="input-field col s12">
+              <i className="material-icons prefix">person</i>
+              <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Name' />
+            </div>
+            <div className="input-field col s12">
+              <i className="material-icons prefix">mail</i>
+              <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='Enter Email' />
+            </div>
+            <div className="input-field col s12">
+              <i className="material-icons prefix">vpn_key</i>
+              <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Enter Password' />
+            </div>
+            <button className="btn waves-effect waves-light right" type="submit" name="action">Submit
+              <i className="material-icons right">send</i>
+            </button>
+          </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='col lg-6 mt-5'>
+        <table className='table'>
+          <thead>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </thead>
+          <tbody>
+            {users.map(user =>
+                  <tr key={user._ID}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.password}</td>
+                    <td>
+                      <button onClick={() => edit(user._ID)} className="btn waves-effect waves-light" type="submit" name="action">
+                        <i className="material-icons ">edit</i>
+                      </button>
+                    </td>
+                    <td>
+                      <button onClick={() => deleteData(user._ID)} className="btn waves-effect waves-light " type="submit" name="action">
+                        <i className="material-icons ">delete</i>
+                      </button>
+                    </td>
+                  </tr>
+                )}
+          </tbody>
+        </table>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </div>  
   )
 }
 
