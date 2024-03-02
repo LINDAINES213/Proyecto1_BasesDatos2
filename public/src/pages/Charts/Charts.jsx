5124fbdbe4da58a212002dbfe9f6f9e1485dadaa
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2024-03-01 18:03:34 -0600

    Cambio de charts de Lab03 a Proyecto1

--- public/src/pages/Charts/Charts.jsx
+++ public/src/pages/Charts/Charts.jsx
@@ -5,26 +5,34 @@ import './Charts.modules.css' // Asegúrate de importar tu hoja de estilos si es
 const Charts = () => {
   useEffect(() => {
     const sdk = new ChartsEmbedSDK({
-      baseUrl: "https://charts.mongodb.com/charts-lab03-pulij", //Actualizar a proyecto1
+      baseUrl: "https://charts.mongodb.com/charts-proyecto1-motby", //Actualizar a proyecto1
     })
 
     const chart1 = sdk.createChart({
-        chartId: "65cbbb79-744d-41a0-8a9d-15cb71214bd4", // REPLACE with the Chart ID
+        chartId: "65e2614b-2540-46e1-8cc7-2d549be2a227", // REPLACE with the Chart ID
         height: "600px",
         width: "900px",
         // Additional options go here
     })
 
     const chart2 = sdk.createChart({
-        chartId: "65cbc24c-5844-45ef-8698-f38daf84bf08", // REPLACE with the Chart ID
+        chartId: "65e26338-865d-46c2-8f66-dc01b6f68828", // REPLACE with the Chart ID
         height: "600px",
         width: "900px",
         // Additional options go here
     })
 
+    const chart3 = sdk.createChart({
+      chartId: "65e26446-28ca-40db-8d12-93713c344c74", // REPLACE with the Chart ID
+      height: "600px",
+      width: "900px",
+      // Additional options go here
+  })
+
 
     chart1.render(document.getElementById("chart1"))
     chart2.render(document.getElementById("chart2"))
+    chart3.render(document.getElementById("chart3"))
 
     
   }, [])
@@ -32,7 +40,8 @@ const Charts = () => {
   return (
     <div className="charts-container">
       <div className="chart" id='chart1'></div>
-      <div className="lastChart"  id='chart2'></div>
+      <div className="midChart"  id='chart2'></div>
+      <div className="lastChart"  id='chart3'></div>
     </div>
   )
 }
